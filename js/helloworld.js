5f2283f71f71bc61e0e0529bf12a02d1502de599
sakurai0124 <s13388ns@zmac107.sfc.keio.ac.jp>
2014-04-24 12:46:42 +0900

    課題用に修正

--- js/helloworld.js
+++ js/helloworld.js
@@ -5,3 +5,12 @@ var helloWorld = function(){
 
 var showHelloWorldButton = document.getElementById("showHelloWorld");
 showHelloWorldButton.addEventListener("click", helloWorld);
+
+
+var helloWorldaaa = function(){
+	var message = document.getElementById("hello");
+	message.setAttribute("class", "");
+};
+
+var showHelloWorldButtonaaa = document.getElementById("HelloWorldbbb");
+showHelloWorldButtonaaa.addEventListener("click", helloWorldaaa);
